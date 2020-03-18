5357ebf111116c7821bd710f036e6363287d3d95
Rawzbeef <43532326+Rawzbeef@users.noreply.github.com>
2020-03-18 18:26:57 +0100

    BéDéDé Neaude
    aime dé R jé sui tro drol

---
+++ BddNode/models/event.js
@@ -0,0 +1,14 @@
+class Event {
+
+    constructor(id, name, date, price) {
+        this.name = name;
+        this.date = date;
+        this.price = price;
+    }
+
+    toString() {
+        return `Event (${this.id},${this.name},${this.date},${this.price})`
+    }
+}
+
+module.exports = Event;
---
+++ BddNode/routes/events.js
@@ -0,0 +1,69 @@
+const bodyParser = require('body-parser');
+const Event = require('../models').Event;
+const eventController = require('../controllers').EventController;
+
+module.exports = function(app) {
+
+
+    app.get('/test', (req, res, next) => {
+        eventController.connect();
+    });
+
+    app.post('/events', bodyParser.json(), (req, res, next) => {
+        if(req.body.name && req.body.date && req.body.price !== undefined) {
+            const event = new Event(undefined,
+                                    req.body.name,
+                                    req.body.date,
+                                    req.body.price);
+            eventController.addEvent(event);
+            res.status(201).end();
+        } else {
+            res.status(400).end();
+        }
+    });
+
+    app.get('/events', (req, res, next) => {
+       //res.json(eventController.findAll());
+       console.log(eventController.findAll());
+    });
+
+    app.get('/events/:id', (req, res, next) => {
+        const id = parseInt(req.params.id);
+        const event = eventController.findById(id);
+        if(!event) {
+            res.status(404).end();
+            return;
+        }
+        res.json(event);
+    });
+
+    app.delete('/events/:id', (req, res, next) => {
+        const id = parseInt(req.params.id);
+        const success = eventController.deleteById(id);
+        if(!success) {
+            res.status(404).end();
+            return;
+        }
+        res.status(204).end();
+    });
+
+    app.put('/events/:id', bodyParser.json(), (req, res, next) => {
+        const id = parseInt(req.params.id);
+        const event = eventController.findById(id);
+        if(!event) {
+            res.status(404).end();
+            return;
+        }
+        if(req.body.name) {
+            event.name = req.body.name;
+        }
+        if(req.body.date) {
+            event.date = req.body.date;
+        }
+        if(req.body.price) {
+            event.price = req.body.price;
+        }
+        res.status(204).end();
+    })
+
+};
